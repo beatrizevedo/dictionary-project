25d3c113b4197282901262a72f28b3be4aaaebf1
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-10-17 15:56:07 +0100

    Added meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,15 +4,14 @@ import './Dictionary.css';
 
 export default function Dictionary() {
   let [word, setWord] = useState('');
+  let [answer, setAnswer] = useState('');
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setAnswer(response.data.meanings[0].definition);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
-
     let apiKey = '049to2dbdcb7ffe54d8674f61d4413a5';
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -31,8 +30,12 @@ export default function Dictionary() {
           placeholder="Tell me the meaning of"
           onChange={handleWordChange}
         />
-        <input type="submit" />
+        <input type="submit" value="Search" className="submit-button" />
       </form>
+      <h2>{word}</h2>
+      <div className="Definition">
+        <p>{answer}</p>
+      </div>
     </div>
   );
 }
