438f7bede10f5a29b96c300a44c5f6062515e978
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-10-22 11:35:52 +0100

    added background image

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import Dictionary from './Dictionary';
 function App() {
   return (
     <div className="App">
-      <main>
-        <Dictionary />
-      </main>
-      <footer>
-        <p className="author">
-          This dictionary was created by Beatriz Azevedo. It's open sourced on
-          Github and hosted on Netlify
-        </p>
-      </footer>
+      <Dictionary />
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,19 +8,28 @@ export default function Dictionary() {
   let [phonetic, setPhonetic] = useState('');
   let [type, setType] = useState('');
   let [example, setExample] = useState('');
+  let [image, setImage] = useState('');
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setAnswer(response.data.meanings[0].definition);
     setPhonetic(response.data.phonetic);
     setType(response.data.meanings[0].partOfSpeech);
     setExample(response.data.meanings[0].example);
   }
 
+  function handleImageResponse(response) {
+    setImage(response.data.photos[0].src.original);
+  }
+
   function search(event) {
     event.preventDefault();
     let apiKey = '049to2dbdcb7ffe54d8674f61d4413a5';
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let apiImageKey = `049to2dbdcb7ffe54d8674f61d4413a5`;
+    let apiImageUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${apiImageKey}`;
+    axios.get(apiImageUrl).then(handleImageResponse);
   }
 
   function handleWordChange(event) {
@@ -29,24 +38,37 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <h1>● Dictionary ● ●</h1>
-      <form className="SearchForm" onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Type your unknown word here"
-          onChange={handleWordChange}
-          className="SearchBox"
-        />
-        <input type="submit" value="Search 🔍" className="SubmitButton" />
-        <small>Search for only 1 word in English</small>
-      </form>
+      <div
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: 'cover',
+        }}
+      >
+        <h1>● Dictionary ● ●</h1>
+        <form className="SearchForm" onSubmit={search}>
+          <input
+            type="search"
+            placeholder="Type your unknown word here"
+            onChange={handleWordChange}
+            className="SearchBox"
+          />
+          <input type="submit" value="Search 🔍" className="SubmitButton" />
+          <div className="instructions">Search for only 1 word in English</div>
+        </form>
 
-      <div className="Definition">
-        <h2 className="Word"> ● {word} ● ●</h2>
-        <p className="Phonetic">{phonetic}</p>
-        <p className="Type">{type}</p>
-        <p className="Answer">{answer}</p>
-        <p className="Example">{example}</p>
+        <div className="Definition">
+          <h2 className="Word"> ● {word} ● ●</h2>
+          <p className="Phonetic">{phonetic}</p>
+          <p className="Type">{type}</p>
+          <p className="Answer">{answer}</p>
+          <p className="Example">{example}</p>
+        </div>
+        <footer>
+          <p className="Author">
+            This dictionary was created by Beatriz Azevedo. It's open sourced on
+            Github and hosted on Netlify
+          </p>
+        </footer>
       </div>
     </div>
   );
