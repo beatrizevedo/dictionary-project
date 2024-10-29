9dcc0b4e18854363e5b39fa3afacc3023e56025f
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-10-29 14:05:32 +0000

    Added image

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,16 +9,19 @@ export default function Dictionary() {
   let [type, setType] = useState('');
   let [example, setExample] = useState('');
   let [image, setImage] = useState('');
+  let [ready, setReady] = useState(true);
 
   function handleDictionaryResponse(response) {
     setAnswer(response.data.meanings[0].definition);
     setPhonetic(response.data.phonetic);
     setType(response.data.meanings[0].partOfSpeech);
     setExample(response.data.meanings[0].example);
+    setReady(true);
   }
 
   function handleImageResponse(response) {
     setImage(response.data.photos[0].src.original);
+    setReady(true);
   }
 
   function search(event) {
@@ -36,40 +39,51 @@ export default function Dictionary() {
     setWord(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <div
-        style={{
-          backgroundImage: `url(${image})`,
-          backgroundSize: 'cover',
-        }}
-      >
-        <h1>● Dictionary ● ●</h1>
-        <form className="SearchForm" onSubmit={search}>
-          <input
-            type="search"
-            placeholder="Type your unknown word here"
-            onChange={handleWordChange}
-            className="SearchBox"
-          />
-          <input type="submit" value="Search 🔍" className="SubmitButton" />
-          <div className="instructions">Search for only 1 word in English</div>
-        </form>
+  if (ready) {
+    return (
+      <div className="Dictionary">
+        <div
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+          }}
+        >
+          <h1>● Dictionary ● ●</h1>
+          <form className="SearchForm" onSubmit={search}>
+            <input
+              type="search"
+              placeholder="Type your unknown word here"
+              onChange={handleWordChange}
+              className="SearchBox"
+            />
+            <input type="submit" value="Search 🔍" className="SubmitButton" />
+            <div className="instructions">
+              Search for only 1 word in English
+            </div>
+          </form>
 
-        <div className="Definition">
-          <h2 className="Word"> ● {word} ● ●</h2>
-          <p className="Phonetic">{phonetic}</p>
-          <p className="Type">{type}</p>
-          <p className="Answer">{answer}</p>
-          <p className="Example">{example}</p>
+          <div className="Definition">
+            <h2 className="Word"> ● {word} ● ●</h2>
+            <p className="Phonetic">{phonetic}</p>
+            <p className="Type">{type}</p>
+            <p className="Answer">{answer}</p>
+            <p className="Example">{example}</p>
+          </div>
+          <footer>
+            <p className="Author">
+              This dictionary was created by Beatriz Azevedo. It's open sourced
+              on Github and hosted on Netlify
+            </p>
+          </footer>
         </div>
-        <footer>
-          <p className="Author">
-            This dictionary was created by Beatriz Azevedo. It's open sourced on
-            Github and hosted on Netlify
-          </p>
-        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="Dictionary">
+        <h1>● Dictionary ● ●</h1>
+        <h2>Refresh and search for another word</h2>
+      </div>
+    );
+  }
 }
